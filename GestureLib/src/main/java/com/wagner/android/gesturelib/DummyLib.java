1bb1fe4705e151c1c98b42c9c80835c170c9894d
Stephan Wagner <stephan.wagner.mi738@gmail.com>
2014-08-23 19:24:14 +0200

    Fix Sample App Add DummyCall to GestureLib Add GestureLibCall to SampleApp

---
+++ GestureLib/src/main/java/com/wagner/android/gesturelib/DummyLib.java
@@ -0,0 +1,14 @@
+package com.wagner.android.gesturelib;
+
+/**
+ * Created by Little on 23.08.2014.
+ */
+public class DummyLib {
+
+    private static final String dummyString = "someDummyString";
+
+    public String callDummyLib(final String message){
+        return "DummyLib Got the following message:" + message;
+
+    }
+}

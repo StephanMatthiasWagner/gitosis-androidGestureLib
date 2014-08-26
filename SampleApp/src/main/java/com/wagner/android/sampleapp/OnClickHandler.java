2a5595f1eafa21dd8d943f45798d14a50babfc0a
Stephan Wagner <stephan.wagner.mi738@gmail.com>
2014-08-26 11:22:30 +0200

    Fix ClickHandling and TextView Update

--- SampleApp/src/main/java/com/wagner/android/sampleapp/OnClickHandler.java
+++ SampleApp/src/main/java/com/wagner/android/sampleapp/OnClickHandler.java
@@ -1,33 +1,40 @@
 package com.wagner.android.sampleapp;
 
+
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import com.wagner.android.gesturelib.DummyLib;
 
 /**
  * This is the ClickHandler for controlling Click events.
  * <p/>
+ *
  * @author of last revision $Author: swagner $
  * @version $Revision: 1.0 $ $Date: 2014/08/16 $
  */
-public class OnClickHandler implements View.OnClickListener
-{
-   private static final String TAG = "OnClickHandler";
+public class OnClickHandler implements View.OnClickListener {
+    private static final String TAG = "OnClickHandler";
 
-   private HelloAndroidActivity activity;
+    /*
+     * The Linked Activity that should be clicked handled-
+     */
+    private HelloAndroidActivity activity;
 
-   public OnClickHandler(final HelloAndroidActivity aActivity)
-   {
-      activity = aActivity;
-   }
+    /**
+     * The Constructor that instantiates OnClickHandler for a given HelloAndroidActivity.
+     * @param aActivity
+     */
+    public OnClickHandler(final HelloAndroidActivity aActivity) {
+        activity = aActivity;
+    }
 
-   @Override
-   public void onClick(final View aView)
-   {
-      final EditText editText = (EditText)activity.findViewById(R.id.edit_message);
-      final TextView outputField = (TextView)activity.findViewById(R.id.textView);
-      outputField.setText(new DummyLib().callDummyLib(editText.getText()));
+    @Override
+    public void onClick(final View aView) {
 
-   }
+        Log.d(TAG, "call onclick");
+        final EditText textInput = (EditText)activity.findViewById(R.id.edit_message);
+        activity.setTextMessage(new DummyLib().callDummyLib("Message from SampleApp:" + textInput.getText().toString()));
+
+    }
 }
